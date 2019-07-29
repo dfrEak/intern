00d269a5cf839a88c3b4dc01868da91bd6b11022
dfrEak <eric.c@gmail.com>
2019-07-29 21:21:31 +0900

    update apply for all area in tokyo

--- webdownload.py
+++ webdownload.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 import math
 
 kwargs = {'project_name': 'tabelog'}
-folder = "C:/Users/bpd-staff/Documents/Eric/download"
+#folder = "C:/Users/bpd-staff/Documents/Eric/download"
+folder = "D:/intern/download/download"
 
 def getPageCount(target_url):
     response = requests.get(target_url)
@@ -22,18 +23,23 @@ def getPageCount(target_url):
     print(pageCount)
     return pageCount
 
+
+
 # from A1301 to A1331 (Tokyo area)
-for area in range(1, 2):
+for area in range(7, 31):
 
     # web url using response
     target_url = "https://tabelog.com/tokyo/A13"+format(area, '02d')
-    #pageCount = getPageCount(target_url)
+    pageCount = getPageCount(target_url)
+    page=61
+    if(pageCount<60):
+        page=pageCount+1
 
     # dummy for area A1301
     #for pageList in range(51,pageCount+1):
     #for pageList in range(43,44):
     # somehow all area only can search page 1 to 60
-    for pageList in range(1,61):
+    for pageList in range(1,page):
         save_webpage(
             url="https://tabelog.com/tokyo/A13"+format(area, '02d')+"/rstLst/"+str(pageList)+"/",
             project_folder=folder,
