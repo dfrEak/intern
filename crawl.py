9652f7f15061a3eafe4c90887bca583461e25cdd
dfrEak <eric.c13@gmail.com>
2019-07-31 14:32:20 +0900

    update new page download and price conversion

--- crawl.py
+++ crawl.py
@@ -5,59 +5,81 @@ import time
 
 folder = "C:/Users/bpd-staff/Documents/Eric/download"
 
+# convert price into 2 price
+# 価格を2つの価格に変換
+def convertPrice(price):
+    # discard "～" char at first
+    # 最初に「〜」文字を破棄
+    if(price[0]=="～"):
+        price=price[1:]
+
+    priceList = price.replace("￥","").replace(",","").split("～")
+    if(len(priceList)==2):
+        return priceList[0],priceList[1]
+    else:
+        return priceList[0],priceList[0]
+
+
 def soupCrawl(target_file):
     # web url using response
+    # レスポンスを使ったウェブURL
     #response = requests.get(target_url)
 
-    # local file use open
-    soup = BeautifulSoup(open(target_file), "html.parser")
+    # ローカルファイル使用オープン
+    soup = BeautifulSoup(open(target_file, encoding="utf-8"), "html.parser")
 
     # list
+    # リスト
     nameList = []
     ratingList = []
     dinnerList = []
     lunchList = []
+    dinnerMinList = []
+    dinnerMaxList = []
+    dinnerAvgList = []
+    lunchAvgList = []
+
 
     # extract restaurant information
+    # レストラン情報の抽出
     restaurants=soup.find_all("div","list-rst__wrap js-open-new-window")
 
-
-
     for r in restaurants:
-        #another method using find
-        #example
-        #if soup.find("div", {"class": "info"}) is not None:
-        #    print("Tag Found")
 
         # name
-        name = r.find_all("a", class_="list-rst__rst-name-target cpy-rst-name")
-        if (len(name) == 1):
-            nameList.append(name[0].text)
+        # 名前
+        name = r.find("a", class_="list-rst__rst-name-target cpy-rst-name")
+        if (name is not None):
+            nameList.append(name.text)
         else:
             nameList.append("-")
 
         # rating
-        rating = r.find_all("span", class_="c-rating__val c-rating__val--strong list-rst__rating-val")
-        if (len(rating) == 1):
-            ratingList.append(rating[0].text)
+        # 評価
+        rating = r.find("span", class_="c-rating__val c-rating__val--strong list-rst__rating-val")
+        if (rating is not None):
+            ratingList.append(rating.text)
         else:
             ratingList.append("-")
 
         # dinner
+        # ディナー
         dinner = r.find_all("span", class_="c-rating__val list-rst__budget-val cpy-dinner-budget-val")
-        if (len(dinner) == 1):
-            dinnerList.append(dinner[0].text)
+        if (dinner is not None):
+            dinnerList.append(dinner.text)
         else:
             dinnerList.append("-")
 
         # lunch
+        # ランチ
         lunch = r.find_all("span", class_="c-rating__val list-rst__budget-val cpy-lunch-budget-val")
-        if (len(lunch) == 1):
-            lunchList.append(lunch[0].text)
+        if (lunch is not None):
+            lunchList.append(lunch.text)
         else:
             lunchList.append("-")
 
-    # looping print
+    # print total information
+    # 合計情報を印刷する
     result=""
     print("name "+str(len(nameList)))
     print("rating "+str(len(ratingList)))
@@ -69,24 +91,56 @@ def soupCrawl(target_file):
         result+=nameList[i]+"\t"+ratingList[i]+"\t"+dinnerList[i]+"\t"+lunchList[i]+"\n"
 
     # remove last \n
+    # 最後に\nを削除
     #result=result[:-1]
     #print(result)
 
     # save file
+    # ファイルを保存
     tools.save("../result.txt",result)
 
     # sleep 1 second
+    # 次のダウンロードに1秒を与える
     #time.sleep(1)
 
 
-# dummy for area A1301 first page
-target_file = folder+"/tabelog/tabelog.com/tokyo/A1301/index.html"
-soupCrawl(target_file)
 
-# dummy for area A1301 2nd page to 60th page
-for pageList in range(2, 61):
-#for pageList in range(3, 4):
-    target_file = folder+"/tabelog/tabelog.com/tokyo/A1301/rstLst/"+str(pageList)+"/index.html"
-    print(pageList)
+def extractWebFull():
+    # area A1301 first page
+    # エリアA1301の最初のページ
+    target_file = folder+"/tabelog/tabelog.com/tokyo/A1301/index.html"
     soupCrawl(target_file)
 
+    # area A1301 2nd page to 60th page
+    # エリアA1301 2〜60ページ
+    for pageList in range(2, 61):
+        target_file = folder+"/tabelog/tabelog.com/tokyo/A1301/rstLst/"+str(pageList)+"/index.html"
+        print(pageList)
+        soupCrawl(target_file)
+
+def extractWebPage():
+    # from A1301 to A1331 (Tokyo area)
+    # A1301から A1331まで（東京エリア）
+    for area in range(1, 2):
+        area_code = "A13"+format(area, '02d')
+        # somehow all area only can search page 1 to 60
+        # どういうわけか全域で1〜60ページしか検索できない
+        for page in range(1, 2):
+            target_file = folder + "/" + area_code + "_" + str(page) + ".html"
+            soupCrawl(target_file)
+            #try:
+                #soupCrawl(target_file)
+            #except:
+                #print(target_file+" not found")
+
+
+
+
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+
+
+extractWebPage()
--- webdownload.py
+++ webdownload.py
@@ -3,57 +3,81 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import math
+from urllib.request import urlopen
 
-kwargs = {'project_name': 'tabelog'}
-#folder = "C:/Users/bpd-staff/Documents/Eric/download"
-folder = "D:/intern/download/download"
+folder = "C:/Users/bpd-staff/Documents/Eric/download"
+#folder = "D:/intern/download/download"
 
+# get page count by calculate all restaurant divided by 20
+# すべてのレストランを20で割って計算してページ数を取得する
 def getPageCount(target_url):
     response = requests.get(target_url)
 
     soup = BeautifulSoup(response.text, "html.parser")
 
     # find the restaurant count using bs4
+    # bs4を使ってレストランの数を探す
     count = soup.find("div", "navi-count").find("strong")
 
     # 1 page 20 results
+    # 1ページ20件
     restaurantCount = int(count.text.replace(',', ''))
     pageCount = math.ceil(restaurantCount/20)
     print(restaurantCount)
     print(pageCount)
     return pageCount
 
+# download the full web (not only page)
+# Web全体をダウンロードする（ページだけでなく）
+def getWebFull(target_url, folder):
+    kwargs = {'project_name': 'tabelog'}
+    save_webpage(
+        url=target_url,
+        project_folder=folder,
+        **kwargs)
+    print("success save : "+target_url)
+    #########################################################
+    #NOTE : Before run, they will open a lot of pages
+    #NOTE : 実行する前に、たくさんのページが開きます
+    #########################################################
 
+# download web page only
+# Webページのみをダウンロードする
+def getWebPage(target_url,name):
+    html = urlopen(target_url).read().decode('utf8')
+    with open(name, 'w', encoding="utf-8") as fid:
+        fid.write(html)
+    print("success save : "+target_url)
+
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
 
 # from A1301 to A1331 (Tokyo area)
-for area in range(8, 32):
+# A1301から A1331まで（東京エリア）
+for area in range(10, 32):
 
     # web url using response
-    target_url = "https://tabelog.com/tokyo/A13"+format(area, '02d')
+    # レスポンスを使ったウェブURL
+    area_code = "A13"+format(area, '02d')
+    target_url = "https://tabelog.com/tokyo/"+area_code
     pageCount = getPageCount(target_url)
-    page=61
+    lastPage=61
     if(pageCount<60):
-        page=pageCount+1
+        lastPage= pageCount + 1
 
-    # dummy for area A1301
-    #for pageList in range(51,pageCount+1):
-    #for pageList in range(43,44):
     # somehow all area only can search page 1 to 60
-    for pageList in range(4,page):
-        save_webpage(
-            url="https://tabelog.com/tokyo/A13"+format(area, '02d')+"/rstLst/"+str(pageList)+"/",
-            project_folder=folder,
-            **kwargs)
+    # どういうわけか全域で1〜60ページしか検索できない
+    for page in range(1, lastPage):
+        url="https://tabelog.com/tokyo/A13" + format(area, '02d') +"/rstLst/" + str(page) + "/"
+
+        #getFullWeb(url,folder)
+        getWebPage(url, folder +"/" + area_code + "_" + str(page) + ".html")
+
         # give 1s to next download
+        # 次のダウンロードに1秒を与える
         time.sleep(1)
 
-#########################################################
-#NOTE : Before run, they will open a lot of pages
-#########################################################
 
-
-#save web only
-#from urllib.request import urlopen
-#html = urlopen("http://www.google.com/")
-#with open('page_content.html', 'w') as fid:
-    #fid.write(html)
