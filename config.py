0ae83cbfe2e1f02d7e1c4d943d4abfc862faf536
atsutoshi <atsutoshi.nakai@cacco.co.jp>
2019-08-02 14:02:22 +0900

    check

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # ref
 # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
-
 import configparser
 from pathlib import Path
 
